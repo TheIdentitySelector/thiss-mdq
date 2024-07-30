d97f08df94e7c849bae5c588c592d39e34365ae1
enriquepablo <enrique@cazalla.net>
2024-07-30 16:50:09 +0200

    more tests

--- test/test_api.js
+++ test/test_api.js
@@ -247,6 +247,142 @@ describe('', () => {
                 done();
             });
         });
+        it('should return 1 IdP, registered by swamid and different from eduID', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(1);
+                    data.forEach(idp => {
+                        chai.expect(idp.registrationAuthority).to.equal("http://www.swamid.se/");
+                        chai.expect(idp.title).to.not.equal("eduID Sweden");
+                        chai.expect(idp).to.not.haveOwnProperty('hint');
+                    });
+                done();
+            });
+        });
+        it('should return all IdPs, trusting only the one registered by swamid and different from eduID', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer2";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(15);
+                    data.forEach(idp => {
+                        if (idp.registrationAuthority !== "http://www.swamid.se/" || idp.title === "eduID Sweden") {
+                            chai.expect(idp).to.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.not.haveOwnProperty('hint');
+                        }
+                    });
+                done();
+            });
+        });
+        it('should return the one IdPs with CoCv1 and different from eduID', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer3";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(1);
+                    data.forEach(idp => {
+                        chai.expect(idp.entity_category).to.include("http://www.geant.net/uri/dataprotection-code-of-conduct/v1");
+                        chai.expect(idp.title).to.not.equal("eduID Sweden");
+                        chai.expect(idp).to.not.haveOwnProperty('hint');
+                    });
+                done();
+            });
+        });
+        it('should return all IdPs trusting only the one with CoCv1 and different from eduID', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer4";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(15);
+                    data.forEach(idp => {
+                        if (!idp.entity_category || !idp.entity_category.includes("http://www.geant.net/uri/dataprotection-code-of-conduct/v1") || idp.title === "eduID Sweden") {
+                            chai.expect(idp).to.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.not.haveOwnProperty('hint');
+                        }
+                    });
+                done();
+            });
+        });
+        it('should return eduID, twice', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer7";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(1);
+                    const idp = data[0];
+                    chai.expect(idp.title).to.equal('eduID Sweden');
+                    chai.request(app)
+                        .get(`/entities/${idp.id}`)
+                        .end((err2,res2) => {
+                            chai.expect(res2.status).to.equal(200);
+                            let idp2 = res2.body;
+                            chai.expect(idp2.title).to.equal('eduID Sweden');
+                    })
+            });
+            done();
+        });
+        it('should return eduID, still twice', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer7";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(1);
+                    const idp = data[0];
+                    chai.expect(idp.title).to.equal('eduID Sweden');
+                    chai.request(app)
+                        .get(`/entities/${idp.id}?entityID=${entityID}&trustProfile=${profile}`)
+                        .end((err2,res2) => {
+                            chai.expect(res2.status).to.equal(200);
+                            let idp2 = res2.body;
+                            chai.expect(idp2.title).to.equal('eduID Sweden');
+                    })
+            });
+            done();
+        });
+        it('should return nothing', (done) => {
+            const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
+            const profile = "customer7";
+            chai.request(app)
+                .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
+                .end((err,res) => {
+                    chai.expect(res.status).to.equal(200);
+                    let data = res.body;
+                    chai.expect(data.length).to.equal(1);
+                    const idp = data[0];
+                    chai.expect(idp.title).to.equal('eduID Sweden');
+                    const profile2 = "customer4";
+                    chai.request(app)
+                        .get(`/entities/${idp.id}?entityID=${entityID}&trustProfile=${profile2}`)
+                        .end((err2,res2) => {
+                            chai.expect(res2.status).to.equal(200);
+                            let idp2 = res2.body;
+                            chai.expect(idp2.title).to.equal('eduID Sweden');
+                    })
+            });
+            done();
+        });
     });
 })
 
