ee2b343426533b0f43bc52a3f144eb79174d1a61
enriquepablo <enrique@cazalla.net>
2024-09-05 16:46:06 +0200

    upgrade dependencies

--- test/test_api.js
+++ test/test_api.js
@@ -35,7 +35,7 @@ describe('', () => {
     });
     describe('GET /', () => {
         it('should return version info', (done) => {
-            chai.request(app)
+            chai.request.execute(app)
                 .get('/')
                 .end((err,res) => {
                   if (err) done(err);
@@ -55,7 +55,7 @@ describe('', () => {
     });
     describe('GET /entities', () => {
         it('should return all entities', (done) => {
-            chai.request(app)
+            chai.request.execute(app)
                 .get('/entities')
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -67,7 +67,7 @@ describe('', () => {
         });
         it('should return 12 IdPs', (done) => {
             const q = "edu";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?q=${q}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -78,7 +78,7 @@ describe('', () => {
         });
         it('should return the SP for 3D Labs SP with a discovery_responses key', (done) => {
             const sp_id = _sha1_id("https://3d.labs.stonybrook.edu/shibboleth");
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities/${sp_id}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -92,7 +92,7 @@ describe('', () => {
         it('should return uleam IdP and 2 IdPs more with registration authority swamid', (done) => {
             const entityID = encodeURIComponent("https://cpauth.icos-cp.eu/saml/cpauth");
             const profile = "customer";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -111,7 +111,7 @@ describe('', () => {
         it('should return usf.edu.br & u-picardie.fr IdPs', (done) => {
             const entityID = encodeURIComponent("https://cpauth.icos-cp.eu/saml/cpauth");
             const profile = "provider";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -127,7 +127,7 @@ describe('', () => {
             const entityID = encodeURIComponent("https://cpauth.icos-cp.eu/saml/cpauth");
             const profile = "provider";
             const q = "picardie";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}&q=${q}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -140,7 +140,7 @@ describe('', () => {
         it('should return all IdPs, trusting only eduid', (done) => {
             const entityID = encodeURIComponent("https://cpauth.icos-cp.eu/saml/cpauth");
             const profile = "other";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -148,9 +148,9 @@ describe('', () => {
                     chai.expect(data.length).to.equal(15);
                     data.forEach(idp => {
                         if (idp.title !== "eduID Sweden") {
-                            chai.expect(idp).to.haveOwnProperty('hint');
-                        } else {
                             chai.expect(idp).to.not.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.haveOwnProperty('hint');
                         }
                     });
                 done();
@@ -160,7 +160,7 @@ describe('', () => {
             const entityID = encodeURIComponent("https://cpauth.icos-cp.eu/saml/cpauth");
             const profile = "other";
             const q = "edu";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}&q=${q}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -168,9 +168,9 @@ describe('', () => {
                     chai.expect(data.length).to.equal(12);
                     data.forEach(idp => {
                         if (idp.title !== "eduID Sweden") {
-                            chai.expect(idp).to.haveOwnProperty('hint');
-                        } else {
                             chai.expect(idp).to.not.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.haveOwnProperty('hint');
                         }
                     });
                 done();
@@ -179,7 +179,7 @@ describe('', () => {
         it('should return 2 IdPs', (done) => {
             const entityID = encodeURIComponent("https://test-edusign.ed-integrations.com/shibboleth");
             const profile = "customer";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -196,7 +196,7 @@ describe('', () => {
             const entityID = encodeURIComponent("https://test-edusign.ed-integrations.com/shibboleth");
             const profile = "customer";
             const q = "edu";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}&q=${q}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -211,7 +211,7 @@ describe('', () => {
         it('should return all IdPs, trusting only those registered by swamid', (done) => {
             const entityID = encodeURIComponent("https://test-edusign.ed-integrations.com/shibboleth");
             const profile = "customer2";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -219,9 +219,9 @@ describe('', () => {
                     chai.expect(data.length).to.equal(15);
                     data.forEach(idp => {
                         if (idp.registrationAuthority !== "http://www.swamid.se/") {
-                            chai.expect(idp).to.haveOwnProperty('hint');
-                        } else {
                             chai.expect(idp).to.not.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.haveOwnProperty('hint');
                         }
                     });
                 done();
@@ -231,7 +231,7 @@ describe('', () => {
             const entityID = encodeURIComponent("https://test-edusign.ed-integrations.com/shibboleth");
             const profile = "customer2";
             const q = "edu";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}&q=${q}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -239,9 +239,9 @@ describe('', () => {
                     chai.expect(data.length).to.equal(12);
                     data.forEach(idp => {
                         if (idp.registrationAuthority !== "http://www.swamid.se/") {
-                            chai.expect(idp).to.haveOwnProperty('hint');
-                        } else {
                             chai.expect(idp).to.not.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.haveOwnProperty('hint');
                         }
                     });
                 done();
@@ -250,7 +250,7 @@ describe('', () => {
         it('should return 1 IdP, registered by swamid and different from eduID', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -267,7 +267,7 @@ describe('', () => {
         it('should return all IdPs, trusting only the one registered by swamid and different from eduID', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer2";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -275,9 +275,9 @@ describe('', () => {
                     chai.expect(data.length).to.equal(15);
                     data.forEach(idp => {
                         if (idp.registrationAuthority !== "http://www.swamid.se/" || idp.title === "eduID Sweden") {
-                            chai.expect(idp).to.haveOwnProperty('hint');
-                        } else {
                             chai.expect(idp).to.not.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.haveOwnProperty('hint');
                         }
                     });
                 done();
@@ -286,7 +286,7 @@ describe('', () => {
         it('should return the one IdPs with CoCv1 and different from eduID', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer3";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -303,7 +303,7 @@ describe('', () => {
         it('should return all IdPs trusting only the one with CoCv1 and different from eduID', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer4";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -311,9 +311,9 @@ describe('', () => {
                     chai.expect(data.length).to.equal(15);
                     data.forEach(idp => {
                         if (!idp.entity_category || !idp.entity_category.includes("http://www.geant.net/uri/dataprotection-code-of-conduct/v1") || idp.title === "eduID Sweden") {
-                            chai.expect(idp).to.haveOwnProperty('hint');
-                        } else {
                             chai.expect(idp).to.not.haveOwnProperty('hint');
+                        } else {
+                            chai.expect(idp).to.haveOwnProperty('hint');
                         }
                     });
                 done();
@@ -322,7 +322,7 @@ describe('', () => {
         it('should return eduID, twice', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer7";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -330,7 +330,7 @@ describe('', () => {
                     chai.expect(data.length).to.equal(1);
                     const idp = data[0];
                     chai.expect(idp.title).to.equal('eduID Sweden');
-                    chai.request(app)
+                    chai.request.execute(app)
                         .get(`/entities/${idp.id}`)
                         .end((err2,res2) => {
                             chai.expect(res2.status).to.equal(200);
@@ -343,7 +343,7 @@ describe('', () => {
         it('should return eduID, still twice', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer7";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -351,7 +351,7 @@ describe('', () => {
                     chai.expect(data.length).to.equal(1);
                     const idp = data[0];
                     chai.expect(idp.title).to.equal('eduID Sweden');
-                    chai.request(app)
+                    chai.request.execute(app)
                         .get(`/entities/${idp.id}?entityID=${entityID}&trustProfile=${profile}`)
                         .end((err2,res2) => {
                             chai.expect(res2.status).to.equal(200);
@@ -364,7 +364,7 @@ describe('', () => {
         it('should return nothing', (done) => {
             const entityID = encodeURIComponent("https://csucoast.infoready4.com/shibboleth");
             const profile = "customer7";
-            chai.request(app)
+            chai.request.execute(app)
                 .get(`/entities?entityID=${entityID}&trustProfile=${profile}`)
                 .end((err,res) => {
                     chai.expect(res.status).to.equal(200);
@@ -373,7 +373,7 @@ describe('', () => {
                     const idp = data[0];
                     chai.expect(idp.title).to.equal('eduID Sweden');
                     const profile2 = "customer4";
-                    chai.request(app)
+                    chai.request.execute(app)
                         .get(`/entities/${idp.id}?entityID=${entityID}&trustProfile=${profile2}`)
                         .end((err2,res2) => {
                             chai.expect(res2.status).to.equal(200);
