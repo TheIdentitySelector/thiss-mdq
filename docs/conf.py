638af6217906ccc77d338a1859a12109809cd39d
enriquepablo <enrique@cazalla.net>
2024-09-11 13:22:27 +0200

    Bump to 1.4.5

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Leif Johansson'
 # The short X.Y version
 version = '1.3'
 # The full version, including alpha/beta/rc tags
-release = '1.4.4'
+release = '1.4.5'
 
 
 # -- General configuration ---------------------------------------------------
